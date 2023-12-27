a1690117aeba75a2280008e1ba3eb8c4ed098d3b
DylanFennelly <dylanantfennelly@gmail.com>
2023-12-27 18:25:53 +0000

    Added testing attraction data and data repository

--- app/src/main/java/com/example/qup/data/Attraction.kt
+++ app/src/main/java/com/example/qup/data/Attraction.kt
@@ -1,5 +1,8 @@
 package com.example.qup.data
 
+import com.google.android.gms.maps.model.LatLng
+
 data class Attraction (
-    var name: String
+    var name: String,       //attraction name
+    var latlng: LatLng      //attraction coordinates
 )
