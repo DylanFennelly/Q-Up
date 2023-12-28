83a2e8597a04c4dd5ae838b9e75545c65e95fca6
DylanFennelly <dylanantfennelly@gmail.com>
2023-12-28 14:59:26 +0000

    Added google Secrets plugin for API Key usage

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,12 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
+
+//https://developers.google.com/maps/documentation/places/android-sdk/secrets-gradle-plugin#kotlin
+//assists with using values placed inside local.properties (api key)
+
+buildscript{
+    dependencies{
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
