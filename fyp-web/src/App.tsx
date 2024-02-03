ce979f95d8875c5cfa5236e93b2144a9e8a59f90
DylanFennelly <dylanantfennelly@gmail.com>
2024-02-03 20:38:52 +0000

    Updated project website

--- fyp-web/src/App.tsx
+++ fyp-web/src/App.tsx
@@ -6,19 +6,43 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+          Dylan Fennelly
+        </h1>
+        <h4>BSc (Hons) in Applied Computing (Cloud & Networks) </h4>
+        <h2>Final Year Project 2024</h2>
+        
+      </header>
+      <body className='App-body'>
+        <h3>Project Poster</h3>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+        > */}
+          <h5>T.B.A.</h5>
+        {/* </a> */}
+        <h3>Demo Video</h3>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        > */}
+          <h5>T.B.A.</h5>
+        {/* </a> */}
+        <h3>Project Code Repository</h3>
+        <a
+          className="App-link"
+          href="https://github.com/DylanFennelly/Q-Up"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Learn React
+          <h5>GitHub</h5>
         </a>
-      </header>
+      </body>
     </div>
   );
 }
