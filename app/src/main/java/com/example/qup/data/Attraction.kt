401d1c4b5d9f9d71cddf412b4905de509048cb98
DylanFennelly <dylanantfennelly@gmail.com>
2024-04-16 14:57:49 +0100

    Added default values for Attraction to prevent API errors

--- app/src/main/java/com/example/qup/data/Attraction.kt
+++ app/src/main/java/com/example/qup/data/Attraction.kt
@@ -4,15 +4,15 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Attraction(
-    val id: Int,                //Internal ID of attraction
-    val name: String,           //Attraction name
-    val description: String,    //Attraction description
-    val type: String,           //Type of attraction (rollercoaster, exhibit, show, restaurant, etc.)
-    val status: String,         //Status of attraction (Open, Maintenance, Closed)
-    val cost: Float,            //Cost of attraction    - TODO: localised currencies
-    val length: Int,          //Length of attraction in seconds
-    val lat: Double,            //Map latitude of attraction
-    val lng: Double,             //Map longitude of attraction
-    val avg_capacity: Int,      //average amount of people on attraction per run
-    val max_capacity: Int       //max amount of people attracition can support
+    val id: Int,                                           //Internal ID of attraction
+    val name: String = "Attraction",                       //Attraction name
+    val description: String = "Attraction Description",    //Attraction description
+    val type: String = "Attraction Type",                  //Type of attraction (rollercoaster, exhibit, show, restaurant, etc.)
+    val status: String = "Closed",                         //Status of attraction (Open, Maintenance, Closed)
+    val cost: Float = 0.00f,                               //Cost of attraction    - TODO: localised currencies
+    val length: Int = 0,                                   //Length of attraction in seconds
+    val lat: Double = 0.0,                                 //Map latitude of attraction
+    val lng: Double = 0.0,                                 //Map longitude of attraction
+    val avg_capacity: Int = 0,                             //average amount of people on attraction per run
+    val max_capacity: Int= 0                               //max amount of people attraction can support
 )
